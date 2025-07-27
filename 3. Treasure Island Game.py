d2fed5eebe1da845f429a925317853ad4695d269
Kaushik <149257323+Kaushik79@users.noreply.github.com>
2025-07-27 15:40:49 +0530

    Update 3. Treasure Island Game.py

--- 3. Treasure Island Game.py
+++ 3. Treasure Island Game.py
@@ -1,3 +1,4 @@
+import random
 print('''
                  ,--"""--,
                .'_       _'.
@@ -15,5 +16,80 @@ print('''
                 \         /
                  '._____.'
     ''')
-print("Welcome to Treasure Island!")
+Name = input("Enter your name: ")
+print(f"{Name}, Welcome to Treasure Island!")
 print("Your mission is to find the treasure.")
+
+roles=["scientist","explorer","solider"]
+r=random.choice(roles)
+print("You are the " + r + ".")
+choice1 = input("You have reached a crossroad, which way would you choose? type l for left and r for right: ")
+if choice1 == "l":
+    print("You have entered a forest\n You found a wounded aggressive bear")
+    if r == "solider":
+        choice2 = input("Type 1 to fight, Type 2 to help, Type 3 to run")
+        if choice2 == "1":
+            print("You defeated the bear💪\n.........................\nYou found a beautiful women lying down and asking for help")
+            choice3 = input("Type 1 to help, Type 2 to run")
+            if choice3 == "2":
+                print("She was one of the bandits.... You escaped safely👍...\n.........................\nYou found a box full of two digit numbers... you randomly picked one number...\n....\n...\n..")
+                num = random.randint(10,99)
+                print(f"You picked {num}\n ......\nYou reached the temple, at the gate you found 3 guards💂‍♂️\n they will let you in if your number can be divided among them equally")
+                if(num%3 == 0):
+                    print("You reached the treasure chest💎")
+                else:
+                    print("☠︎︎ You were killed by guards ☠︎︎")
+            else:
+                print("You were tricked, she was one of the bandits\n☠︎︎ You were killed by the Bandits ☠︎︎")
+        else:
+            print("☠︎︎ You were killed by the bear ☠︎︎")
+    elif r == "explorer":
+        choice2 = input("Type 1 to fight, Type 2 to help, Type 3 to run")
+        if choice2 == "3":
+            print(
+                "You escaped safely🏃\n.........................\nYou found a beautiful women lying down and asking for help")
+            choice3 = input("Type 1 to help, Type 2 to run")
+            if choice3 == "2":
+                print(
+                    "She was one of the bandits.... You escaped safely👍...\n.........................\nYou found a box full of two digit numbers... you randomly picked one number...\n....\n...\n..")
+                num = random.randint(10, 99)
+                print(
+                    f"You picked {num}\n ......\nYou reached the temple, at the gate you found 3 guards💂‍♂️\n they will let you in if your number can be divided among them equally")
+                if (num % 3 == 0):
+                    print("You reached the treasure chest💎")
+                else:
+                    print("☠︎︎ You were killed by guards ☠︎︎")
+            else:
+                print("You were tricked, she was one of the bandits\n☠︎︎ You were killed by the Bandits ☠︎︎")
+        else:
+            print("☠︎︎ You were killed by the bear ☠︎︎")
+    elif r == "scientist":
+        choice2 = input("Type 1 to fight, Type 2 to help, Type 3 to run")
+        if choice2 == "2":
+            print(
+                "You healed the bear❤️‍🩹\nA loving lick from the bear 💖\n.........................\nYou found a beautiful women lying down and asking for help")
+            choice3 = input("Type 1 to help, Type 2 to run")
+            if choice3 == "2":
+                print(
+                    "She was one of the bandits.... You escaped safely👍...\n.........................\nYou found a box full of two digit numbers... you randomly picked one number...\n....\n...\n..")
+                num = random.randint(10, 99)
+                print(
+                    f"You picked {num}\n ......\nYou reached the temple, at the gate you found 3 guards💂‍♂️\n they will let you in if your number can be divided among them equally")
+                if (num % 3 == 0):
+                    print("You reached the treasure chest💎")
+                else:
+                    print("☠︎︎ You were killed by guards ☠︎︎")
+            else:
+                print("You were tricked, she was one of the bandits\n☠︎︎ You were killed by the Bandits ☠︎︎")
+        else:
+            print("☠︎︎ You were killed by the bear ☠︎︎")
+elif choice1 == "r":
+    print()
+
+else:
+    print("Invalid input. Please try again.")
+
+
+
+
+
